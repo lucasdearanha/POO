dde7bff6d7229509b4c55bcdcb681c5c41361bf8
lucasdearanha <lucasaranha056@gmail.com>
2021-12-26 10:51:29 -0300

    price tags project

---
+++ PriceTags/PriceTags/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using PriceTags.Entities;
+using System.Collections.Generic;
+
+namespace PriceTags
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var products = new List<Product>();
+
+            Console.Write("Enter de number of products: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Product #{i+1} data:");
+                Console.Write("Common, used or imported (c, u, i)? ");
+                char option = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (option == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                } else if (option == 'u')
+                {
+                    Console.Write("Manufacture date: ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                } else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
